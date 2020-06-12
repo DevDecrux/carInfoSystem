44bc9c6dfe8fea4613d3723de9adbcc7b90142fc
decrux <milen.ovalchev@gmail.com>
2020-06-12 10:01:50 +0300

    init new backend spring boot

---
+++ car-info-core/src/test/java/com/tu/carinfocore/CarinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tu.carinfocore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
